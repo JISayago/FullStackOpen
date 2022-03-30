8774f961df06c4e02a9718fe2b6bc8fde5e1d7b0
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2022-03-30 12:08:07 -0300

    parte2-1

---
+++ part2/notes/src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Note from "./Note.js"
+
+const App = props => {
+    const { notes } = props
+  
+    return (
+      <div>
+        <h1>Notes</h1>
+        <ul>
+        {
+          notes.map(
+            note =>
+            <Note key={note.id} note={note}/>
+          )
+        }
+        </ul>
+      </div>
+    )
+  }
+
+  export default App
